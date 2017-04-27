a6a2c9318c1d36bb6ad3f310d58e00155c8f2499
Jordan Forbes <Jordforb@gmail.com>
2017-04-27 11:43:18 -0400

    created notes js

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = function(w,h){
+	var hi = require('./notes.js')
+	return w * h +hi("hi")
+	
+};
---
+++ notes.js
@@ -0,0 +1,12 @@
+module.exports=function(){
+
+var notes=["a","as","b","c","cs","d","ds","e","f","fs","g","gs"]
+
+x=10;
+y=11;
+z=12;
+
+return notes[x]
+
+
+};
